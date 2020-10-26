72b7f2e55424ad9888bcf25e71fa39a2b5c13c99
IAmArien <palisocnorman@gmail.com>
2020-10-27 00:49:17 +0800

    Updates

---
+++ assets/js/filter.js
@@ -0,0 +1,20 @@
+//Used Isotope plugin for filtering 
+// https://github.com/metafizzy/isotope
+
+window.onload = function() {
+    var $container = $('.animate-grid .gallary-thumbs');
+    $container.isotope({
+        filter: '*',
+        transitionDuration: '0.6s',
+    });
+    $('.animate-grid .categories a').click(function() {
+        $('.animate-grid .categories .active').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector
+        });
+        return false;
+    });
+}
+
---
+++ precache-manifest.42abb399a0459ce3e6838ff85d7aae24.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678b042ffa6c319242a87ebcdf70d889",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "3e9be35f57a22b2b344d",
+    "url": "/me/static/css/2.40c68a69.chunk.css"
+  },
+  {
+    "revision": "d1fb15105c89ed47b460",
+    "url": "/me/static/css/main.c96a0748.chunk.css"
+  },
+  {
+    "revision": "3e9be35f57a22b2b344d",
+    "url": "/me/static/js/2.7735a848.chunk.js"
+  },
+  {
+    "revision": "a7aec927f1e863a53de22e58e5329447",
+    "url": "/me/static/js/2.7735a848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1fb15105c89ed47b460",
+    "url": "/me/static/js/main.97baa2b8.chunk.js"
+  },
+  {
+    "revision": "99f554fa25fa276fb8e1",
+    "url": "/me/static/js/runtime-main.3d005c59.js"
+  },
+  {
+    "revision": "2eada6e9ded34ce65916453a7d0669f2",
+    "url": "/me/static/media/AGRO_MODERN_MOCKUP_SCS.2eada6e9.png"
+  },
+  {
+    "revision": "b44b412316e6b1c8268e385a1d582e03",
+    "url": "/me/static/media/BAMASK_MOCKUP_SCS.b44b4123.png"
+  },
+  {
+    "revision": "c079100604dda169ae6f01c2c41b34e5",
+    "url": "/me/static/media/BEULPING_MP3_MOCKUP_SCS.c0791006.png"
+  },
+  {
+    "revision": "08e26e58cf40671f5ec5cc874f2cecc7",
+    "url": "/me/static/media/CRMS_MOCKUP_SCS.08e26e58.png"
+  },
+  {
+    "revision": "e1be69ba60f4447d2a1838fe5845c636",
+    "url": "/me/static/media/Hackathon_2019.e1be69ba.png"
+  },
+  {
+    "revision": "d30fc1ddceb0b91e22b095d75d6caced",
+    "url": "/me/static/media/Hackathon_2020.d30fc1dd.png"
+  },
+  {
+    "revision": "675ea9168b0651501359b56e64f38f62",
+    "url": "/me/static/media/IAMARIEN.675ea916.png"
+  },
+  {
+    "revision": "19400f4064e000290eb6ae4411017e35",
+    "url": "/me/static/media/MK_MOCKUP_SCS.19400f40.png"
+  },
+  {
+    "revision": "ca17517891ac328e03156ddf2666015b",
+    "url": "/me/static/media/NP_Resume.ca175178.pdf"
+  },
+  {
+    "revision": "7a82944482f6f744bf6237d68a70cd58",
+    "url": "/me/static/media/PMD_MOCKUP_SCS.7a829444.png"
+  },
+  {
+    "revision": "b2648d7f9fdd3931fd076f8f161779b1",
+    "url": "/me/static/media/PNPCPS_MOCKUP_SCS.b2648d7f.png"
+  },
+  {
+    "revision": "787d5ff42fa3b643031f240d71b1eb12",
+    "url": "/me/static/media/TBA_MOCKUP_SCS.787d5ff4.png"
+  },
+  {
+    "revision": "f64a4992ce5f3d3893e654e2c200309d",
+    "url": "/me/static/media/Talk.f64a4992.png"
+  },
+  {
+    "revision": "62da8ba91bc003c3705519dc55095997",
+    "url": "/me/static/media/WDN_ADMIN_MOCKUP_SCS.62da8ba9.jpg"
+  },
+  {
+    "revision": "830d7ca2ec99a975db9f83845668036b",
+    "url": "/me/static/media/ai_pilipinas.830d7ca2.jpg"
+  },
+  {
+    "revision": "6340bddce9d375775fe324b24fed898d",
+    "url": "/me/static/media/best_iot.6340bddc.jpg"
+  },
+  {
+    "revision": "d7fee844e1f5af5565f24de2983d50ff",
+    "url": "/me/static/media/best_lead.d7fee844.jpg"
+  },
+  {
+    "revision": "ba914e94fe20716737de80ea2f3cf650",
+    "url": "/me/static/media/best_thesis.ba914e94.jpg"
+  },
+  {
+    "revision": "24d40d0e2f8a831567d48439b0b478f8",
+    "url": "/me/static/media/greenbowl.24d40d0e.jpg"
+  },
+  {
+    "revision": "7168504351df67c789afaf904eac5a9c",
+    "url": "/me/static/media/impact_hub.71685043.jpg"
+  },
+  {
+    "revision": "fc50f4459b58e8d4605db3d8db17248d",
+    "url": "/me/static/media/laravel_philippines.fc50f445.jpg"
+  },
+  {
+    "revision": "56108512d496a8af9357b81aeddcdf0c",
+    "url": "/me/static/media/outstanding_ios.56108512.jpg"
+  },
+  {
+    "revision": "d5efcab770b76717134a31e2733db296",
+    "url": "/me/static/media/profile.d5efcab7.jpg"
+  },
+  {
+    "revision": "8d2ddf445de566247366a1b0ecc4772b",
+    "url": "/me/static/media/pyscript.8d2ddf44.png"
+  },
+  {
+    "revision": "91d9f2039167ae2f1f653621be9dc678",
+    "url": "/me/static/media/python_philippines.91d9f203.jpg"
+  },
+  {
+    "revision": "2a1973dd1969d8e9386c3a6c595ba11e",
+    "url": "/me/static/media/tensorflow_developers.2a1973dd.jpg"
+  },
+  {
+    "revision": "bb3960efd86478ab3dfa2566f1dbf1df",
+    "url": "/me/static/media/vue_js_philippines.bb3960ef.jpg"
+  }
+]);
